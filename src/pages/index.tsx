52f30a5f7edd1d94bf2178677f8a0ab692756fb1
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-04-19 15:16:22 +0200

    Added tailwind and installed prisma

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col p-8">
+      <p className="text-2xl">Hello</p>
+    </div>
+  )
+};
+
+export default Home
